e0e3b1eeb609a2c0cdb016bd24a8b3faae07e70b
howard otuya <howardotuya17@gmail.com>
2023-08-07 00:02:54 +0100

    updated Landing page: Added 'Life Outside' section

--- app/page.js
+++ app/page.js
@@ -376,7 +376,7 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <div className="flex pt-8 lg:pt-[120px] w-full items-center justify-center">
+              <div className="flex pt-10 lg:pt-[120px] w-full items-center justify-center">
                 <div
                   id="CTA"
                   class="py-2 px-4 rounded-[20px] w-auto flex justify-center items-center gap-1 text-xs g10 border border-[#fafafa]"
