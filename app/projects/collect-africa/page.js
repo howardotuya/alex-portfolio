3b06593a39b6e6d6817e5a62440d4268542865c3
howard otuya <howardotuya17@gmail.com>
2023-08-14 13:23:14 +0100

    updated collect africa

--- app/projects/collect-africa/page.js
+++ app/projects/collect-africa/page.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import arrowleft from "@/public/images/arrow-left.png";
 import headerlg from "@/public/images/collect-africa/header-lg.png";
 import headersm from "@/public/images/collect-africa/header-sm.png";
+import challenge from "@/public/images/collect-africa/challenge.png";
+import despro from "@/public/images/collect-africa/despro.svg";
 
 function Collect() {
   return (
@@ -35,7 +37,7 @@ function Collect() {
               className=" w-full h-auto"
               placeholder="blur"
               src={headerlg}
-              alt=""
+              alt="collect africa"
             />
           </div>
           <div className="flex lg:hidden flex-col justify-center items-center">
@@ -43,15 +45,15 @@ function Collect() {
               className="w-full h-auto"
               placeholder="blur"
               src={headersm}
-              alt=""
+              alt="collect africa"
             />
           </div>
         </div>
       </header>
 
       <main className="minusSect bg-[#252627] setbody">
-        <div className="px-5 flex flex-col gap-20 lg:gap-[120px] pt-20 lg:pt-[120px] lg:pl-[238px] lg:pr-[244px]">
-          <section class="highlights">
+        <div className="flex flex-col gap-20 lg:gap-[120px] pt-20 lg:pt-[120px]">
+          <section class="highlights ms-padding">
             <h2 class="phc-heading2">Highlights</h2>
             <div className="phc-h-cont">
               <div className="w-full lg:w-auto">
@@ -69,8 +71,8 @@ function Collect() {
             </div>
           </section>
 
-          <section class="TheChallenge">
-            <div className="phc-bcont">
+          <section class="theChallenge">
+            <div className="phc-bcont ms-padding">
               <h2 className="phc-heading2">The Challenge</h2>
               <p className="phc-bodyp">
                 <span>
@@ -89,7 +91,60 @@ function Collect() {
                 </span>
               </p>
             </div>
-            <div></div>
+            <div className="phc-bimage nopad">
+              <div className="flex flex-col justify-center items-center">
+                <Image
+                  className="w-full h-auto"
+                  src={challenge}
+                  alt="Collect Africa"
+                  placeholder="blur"
+                />
+              </div>
+            </div>
+          </section>
+
+          <section class="theObjective ms-padding">
+            <div className="phc-bcont">
+              <h2 className="phc-heading2">The Objective</h2>
+              <p className="phc-bodyp">
+                <span>
+                  The objective was to develop a unique solution that would
+                  target businesses and provide them with a one-stop solution to
+                  assist them manage their financial operations, make sending
+                  money anywhere in the globe simple and make receiving money
+                  from clients in any location as effortless as possible.
+                </span>
+                <span>
+                  We want to make it easier for businesses with multiple
+                  locations to manage their finances and swiftly account for all
+                  of their transactions in one location rather than always
+                  having to visit the bank and go through the tedious signatory
+                  to account routine. You may quickly monitor and balance your
+                  account with its assistance, growing and elevating your
+                  company as a result.
+                </span>
+                <span>
+                  Last but not least, what is a business without customers? We
+                  want to assist you in managing and building relationships with
+                  them.
+                </span>
+              </p>
+            </div>
+          </section>
+
+          <section class="designProcess">
+            <div className="phc-bcont ms-padding">
+              <h2 className="phc-heading2">Design Process</h2>
+            </div>
+            <div className="phc-bimage nopad">
+              <div className="flex overflow-hidden lg:rounded-[25px]">
+                <Image
+                  className="w-full h-auto"
+                  src={despro}
+                  alt="Design Process"
+                />
+              </div>
+            </div>
           </section>
         </div>
       </main>
