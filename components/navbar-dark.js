32ea4603249674d9b99e9f7e0b9eed4c55f4de7b
howard otuya <howardotuya17@gmail.com>
2023-08-16 19:14:32 +0100

    updated navbar

--- components/navbar-dark.js
+++ components/navbar-dark.js
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import close from "@/public/images/x-close.png";
 import { useEffect } from "react";
 
@@ -15,27 +16,29 @@ function NavbarDark() {
     } else {
       popup.style.display = "none";
     }
-  };
+  }; 
   return (
     <>
-      <div className="g20 xpopup fixed hidden flex-col justify-between px-5 pt-8 pb-14 top-0 bottom-0 left-0 right-0 z-[999999999]">
+      <div className="g20 xpopup fixed hidden flex-col gap-20 justify-between px-5 pt-8 pb-14 top-0 bottom-0 left-0 right-0 z-[999999999]">
         <div className="flex flex-col gap-14">
           <nav className="w-full text-xs font-bold uppercase flex flex-row justify-between items-center">
-            <h6 className="g700">Alex Oyebade</h6>
+            <Link href="/" onClick={popuphandler}>
+              <h6 className="g700">Alex Oyebade</h6>
+            </Link>
             <div className="flex lg:hidden items-center justify-center flex-col w-6 h-6">
               <Image
                 onClick={popuphandler}
                 className="w-full h-auto"
                 src={close}
                 placeholder="blur"
-                quality={100} 
+                quality={100}
                 alt="close"
-              /> 
+              />
             </div>
           </nav>
           <nav>
             <ul className="text-[#6C6D6D] flex flex-col gap-8 text-xs font-semibold uppercase leading-4">
-              <li className="underline">Contact me</li>
+             <Link href="mailto:ao.dsgnr@gmail.com"><li className="underline">Contact me</li></Link>
               <li>works</li>
               <li>articles</li>
             </ul>
@@ -45,21 +48,26 @@ function NavbarDark() {
         <div className="sidebar-bttm-top">
           <div className="sidebar-bttm">
             <ul className="d-120">
-              <li>resumé</li>
-              <li>Linkedin</li>
-              <li>TWitter</li>
-              <li>dribble</li>
+            <Link href="https://read.cv/join/alexoyebade"><li>resumé</li></Link>
+            <Link href="https://www.linkedin.com/in/iamalexoyebade/"><li>Linkedin</li></Link>
+            <Link href="https://twitter.com/iamalexoyebade"><li>TWitter</li></Link>
+            <Link href="https://dribbble.com/AlexOye"><li>dribble</li></Link>
             </ul>
             <div className="d-130">copyright 2023 alex oyebade</div>
           </div>
         </div>
       </div>
+
       <div className="setbody bg-[#161718] sticky top-0 left-0 right-0 z-[99999]">
         <div className="grey-g20 text-[#F5F5F5]">
           <nav className="px-5 lg:px-6 pt-8 lg:pt-12 pb-[22px] w-full text-xs font-bold uppercase flex flex-row justify-between items-center">
-            <h6 className="">Alex Oyebade</h6>
+            <Link href="/">
+              <h6>Alex Oyebade</h6>
+            </Link>
             <ul className="hidden lg:flex items-center gap-6">
-              <li className="underline">Contact me</li>
+              <Link href="mailto:ao.dsgnr@gmail.com">
+                <li className="underline">Contact me</li>
+              </Link>
               <li className=" g70">works</li>
               <li className=" g70">articles</li>
             </ul>
--- components/navbar-light.js
+++ components/navbar-light.js
@@ -1,30 +1,32 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import menu from "@/public/images/menu.png";
 import close from "@/public/images/x-close.png";
 import { useEffect } from "react";
 
 function NavbarLight() {
-
   useEffect(() => {
     document.querySelector(".xpopup").style.display = "none";
-  })
+  });
 
   const popuphandler = () => {
     const popup = document.querySelector(".xpopup");
     if (popup.style.display == "none") {
       popup.style.display = "flex";
-    } else { 
+    } else {
       popup.style.display = "none";
     }
   };
 
   return (
     <>
-      <div className="g20 xpopup fixed hidden flex-col justify-between px-5 pt-8 pb-14 top-0 bottom-0 left-0 right-0 z-[999999999]">
+      <div className="g20 xpopup fixed hidden flex-col gap-20 justify-between px-5 pt-8 pb-14 top-0 bottom-0 left-0 right-0 z-[999999999]">
         <div className="flex flex-col gap-14">
           <nav className="w-full text-xs font-bold uppercase flex flex-row justify-between items-center">
-            <h6 className="g700">Alex Oyebade</h6>
+            <Link href="/" onClick={popuphandler}>
+              <h6 className="g700">Alex Oyebade</h6>
+            </Link>
             <div className="flex lg:hidden items-center justify-center flex-col w-6 h-6">
               <Image
                 onClick={popuphandler}
@@ -38,7 +40,9 @@ function NavbarLight() {
           </nav>
           <nav>
             <ul className="text-[#6C6D6D] flex flex-col gap-8 text-xs font-semibold uppercase leading-4">
-              <li className="underline">Contact me</li>
+              <Link href="mailto:ao.dsgnr@gmail.com">
+                <li className="underline">Contact me</li>
+              </Link>
               <li>works</li>
               <li>articles</li>
             </ul>
@@ -48,10 +52,18 @@ function NavbarLight() {
         <div className="sidebar-bttm-top">
           <div className="sidebar-bttm">
             <ul className="d-120">
-              <li>resumé</li>
-              <li>Linkedin</li>
-              <li>TWitter</li>
-              <li>dribble</li>
+              <Link href="https://read.cv/join/alexoyebade">
+                <li>resumé</li>
+              </Link>
+              <Link href="https://www.linkedin.com/in/iamalexoyebade/">
+                <li>Linkedin</li>
+              </Link>
+              <Link href="https://twitter.com/iamalexoyebade">
+                <li>TWitter</li>
+              </Link>
+              <Link href="https://dribbble.com/AlexOye">
+                <li>dribble</li>
+              </Link>
             </ul>
             <div className="d-130">copyright 2023 alex oyebade</div>
           </div>
@@ -61,9 +73,13 @@ function NavbarLight() {
       <div class="setbody g20 fixed top-0 left-0 right-0 z-[99999]">
         <div>
           <nav className="px-5 lg:px-6 pt-8 lg:pt-12 pb-[22px] w-full text-xs font-bold uppercase flex flex-row justify-between items-center">
-            <h6 className="g700">Alex Oyebade</h6>
+            <Link href="/">
+              <h6 className="g700">Alex Oyebade</h6>
+            </Link>
             <ul className="hidden lg:flex items-center g900 gap-6">
-              <li className="underline">Contact me</li>
+              <Link href="mailto:ao.dsgnr@gmail.com">
+                <li className="underline">Contact me</li>
+              </Link>
               <li>works</li>
               <li>articles</li>
             </ul>
