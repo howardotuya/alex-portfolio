b387873974acaa7fa6b88dab87108977f8381466
howard otuya <howardotuya17@gmail.com>
2023-08-20 22:01:21 +0100

    Updated Zacrac Learning

--- app/projects/zacrac-learning/page.js
+++ app/projects/zacrac-learning/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import NavbarLight from "@/components/navbar-light";
-import Img1 from "@/public/images/zacrac-learning/img1.png";
+import Img1 from "@/public/images/zacrac-learning/Img1.png";
+import Img2 from "@/public/images/zacrac-learning/Img2.png";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
 
       <header className="setbody g30-alt pt-[184px] lg:pt-[246px] pb-20 lg:pb-[147px] ">
         <div className="no-use">
-          <div className="header px-6">
+          <div className="header px-5 lg:px-6">
             <div className="zheader-top1">
               <Link href="/" className="fheader-button">
                 <div>
@@ -31,7 +32,7 @@ export default function Home() {
                 </div>
                 <span>Back to home</span>
               </Link>
-              <div className="max-w-[766px]">
+              <div className="max-w-[300px] lg:max-w-[766px]">
                 <Image
                   className="w-full h-auto"
                   src={Img1}
@@ -40,22 +41,74 @@ export default function Home() {
                 />
               </div>
             </div>
-            <div className="zheader-top2 pt-16">
+            <div className="zheader-top2 pt-8 lg:pt-16">
               <h1>Learn. Grow. Thrive.</h1>
               <p>
-                Zacrac Learning is the leading online educational platform
-                providing students with world class courses in data science and
-                analytics.
+                <span className="glow-z">Zacrac Learning</span> is the leading
+                online educational platform providing students with world class
+                courses in data science and analytics.
               </p>
             </div>
-            <div className="pt-[216px]">
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="pt-10 lg:pt-[216px] zheader-top3">
+              <div>
+                <h6>Client</h6>
+                <p>
+                  <span>Zacrac Learning</span>
+                  <span>August 2022 - Oct 2022</span>
+                </p>
+              </div>
+              <div>
+                <h6>Contribution</h6>
+                <p>
+                  <span>Website Redesign</span>
+                </p>
+              </div>
+              <div>
+                <h6>Team</h6>
+                <p>
+                  <span>Wale Adeyemo</span>
+                  <span>Kemi Oluwakemi</span>
+                  <span>David Sawe</span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </header>
+
+      <section className="g30-alt setbody">
+        <div>
+          <div className="w-full grid justify-items-center items-center">
+            <div className="w-full grid justify-items-center items-center px-5 lg:px-[82px] pt-20 lg:pt-[144px]">
+              <Image
+                className="w-full h-auto"
+                src={Img2}
+                alt="Zacrac Landing Page"
+                placeholder="blur"
+                quality={100}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="g30-alt setbody">
+        <div>
+          <div className="w-full py-20 lg:pt-[216px] lg:pb-[249px]">
+            <div className="fheader-bottom">
+              <h6>filling the unemployment gap</h6>
+              <p className="text-[#252627!important] max-w-[1283px!important]">
+                At 37% unemployment rate in Nigeria 2023 (NESG), we are
+                desperately in need of innovative educational solutions for the
+                teeming Nigerian youthful population.{" "}
+                <span className="text-[#898A8A]">
+                  Zooms in Zacrac Learning. 🚀
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
