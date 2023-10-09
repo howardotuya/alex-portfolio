10eafac0f0e5d5afa443658e4387cd19abac9fe8
Howard Otuya <howardotuya17@gmail.com>
2023-10-09 19:42:42 +0100

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 export const metadata = {
@@ -145,6 +146,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
